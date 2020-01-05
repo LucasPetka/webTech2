e46e982d8b12cc985d8ca9d2be8d17948586805e
PeatMan <lucas.petka@gmail.com>
2020-01-05 12:12:43 +0200

    Work at home

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,14 +2,19 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 
 import BootstrapVueTreeview from 'bootstrap-vue-treeview'
 import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.component('hierarchy', require('./components/Hierarchy.vue').default);
 Vue.component('vue-bootstrap-typeahead', VueBootstrapTypeahead);
 
 
+Vue.use(BootstrapVue)
 Vue.use(BootstrapVueTreeview);
 
 
--- resources/js/components/Hierarchy.vue
+++ resources/js/components/Hierarchy.vue
@@ -33,7 +33,7 @@
 
 
                              <div v-if="subClasses != null  && subClasses.length != 0"> 
-                                <b-tree-view :data="subClasses" :contextMenuItems="items" @contextMenuItemSelect="menuItemSelected" ref="tree"></b-tree-view>
+                                <b-tree-view :data="subClasses" :renameNodeOnDblClick="false" :contextMenuItems="items" @contextMenuItemSelect="menuItemSelected" ref="tree"></b-tree-view>
                              </div>  
 
                         <!-- Update Modal -->
@@ -53,13 +53,13 @@
 
                                     
                                         <div class="form-group">
-                                            <label for="nameInput">Name</label>
-                                            <input name="node_name" type="name" class="form-control" id="nameInput" placeholder="Enter Node name" v-model="nodee.name" required>
+                                            <label for="nameInput_update">Name</label>
+                                            <input name="node_name" type="name" class="form-control" id="nameInput_update" placeholder="Enter Node name" v-model="nodee.name" required>
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="exampleFormControlSelect1">Select Parent</label>
-                                            <select name="node_parent" class="form-control" id="exampleFormControlSelect1" v-model="nodee.pid" required>
+                                            <label for="node_parent_update">Select Parent</label>
+                                            <select name="node_parent" class="form-control" id="node_parent_update" v-model="nodee.pid" required>
                                             <option v-for="classh in allClasses" v-bind:key="classh.cid" :value="classh.cid" >{{ classh.name }}</option>
                                             </select>
                                         </div>
@@ -88,26 +88,23 @@
                                     </button>
                                 </div>
                                 <div class="modal-body">
-
-                                    <p>Node name: {{ nodee.name }}<br>Node Parent ID: {{ nodee.pid }}</p>
-
                                     
                                         <div class="form-group">
-                                            <label for="nameInput">Name</label>
-                                            <input name="node_name" type="name" class="form-control" id="nameInput" placeholder="Enter Node name" v-model="nodee.name" required>
+                                            <label for="nameInput_add">Name</label>
+                                            <input name="node_name" type="name" class="form-control" id="nameInput_add" placeholder="Enter Node name" v-model="nodee.name" required>
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="exampleFormControlSelect1">Select Parent</label>
-                                            <select name="node_parent" class="form-control" id="exampleFormControlSelect1" v-model="nodee.pid" required>
+                                            <label for="node_parent_add">Select Parent</label>
+                                            <select name="node_parent" class="form-control" id="node_parent_add" v-model="nodee.pid" required>
                                             <option :value="0" >**Super Parent**</option>
                                             <option v-for="classh in allClasses" v-bind:key="classh.cid" :value="classh.cid" >{{ classh.name }}</option>
                                             </select>
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="exampleFormControlSelect1">Abstract class</label>
-                                            <select name="node_parent" class="form-control" id="exampleFormControlSelect1" v-model="nodee.abstract" required>
+                                            <label for="abstract_add">Abstract class</label>
+                                            <select name="abstract" class="form-control" id="abstract_add" v-model="nodee.abstract" required>
                                             <option :value="1">Yes</option>
                                             <option :value="0">No</option>
                                             </select>
@@ -129,6 +126,32 @@
 
                     </div>
                 </div>
+
+                <h3 class="mt-3"> Parent classes rows </h3>
+                <hr>
+
+                <div class="overflow-auto mt-2">
+                    <b-table
+                    id="my-table"
+                    :items="itemss"
+                    :per-page="perPage"
+                    :current-page="currentPage"
+                    small
+                    ></b-table>
+
+                    <p class="mt-3 text-center">Current Page: {{ currentPage }}</p>
+
+                    <b-pagination
+                    v-model="currentPage"
+                    :total-rows="rows"
+                    :per-page="perPage"
+                    aria-controls="my-table"
+                    align="center"
+                    ></b-pagination>
+                </div>
+
+
+
             </div>
         </div>
     </div>
@@ -139,7 +162,17 @@
     export default {
         mounted() {
             this.fetchAllClasses();
+        },
 
+        computed: {
+            rows() {
+                try {
+                    return this.itemss.length
+                } catch (error) {
+                    return 0
+                }
+                 
+            }
         },
 
         data() {
@@ -174,34 +207,44 @@
                     pid:"",
                     abstract:""
                 },
+
+                perPage: 3,
+                currentPage: 1,
+                itemss: []
             }
         },
 
         methods:{
+
+            linkGen(pageNum) {
+                return pageNum === 1 ? '?' : `?page=${pageNum}`
+            },
 
 
             fetchSubClasses: async function(classId){
                 this.classCid = classId;
                 await axios.get('/api/subclasses/' + classId)
                     .then(response => (this.subClasses = response.data));
-
+                    
+                    this.$refs.tree.nodeMap = undefined;
                     this.$refs.tree.selectedNode = null;
                     this.$refs.tree.createNodeMap();        //testing class refresh
-                
-                    
-                if(this.$refs.tree){
-                 console.log(this.$refs.tree);  
-                }
-
             },
 
             fetchSuperClasses: async function(classId){
+                this.superClasses = [];
                 await axios.get('/api/superclasses/' + classId)
                     .then(response => (this.superClasses = response.data));
+
+                    this.superClasses.list.forEach(classh => {
+                        this.itemss.push({Name : classh.name, Parent : this.getParentNameById(classh.pid)})
+                    });         
 
                     var openClasses = this.superClasses.list.reverse();
                     var saveClass;
                     openClasses.forEach(classh => {
+
+                        this.$refs.tree.createNodeMap();
                         if(this.$refs.tree.getNodeByKey((classh.cid).toString())){
                             this.$refs.tree.getNodeByKey((classh.cid).toString()).expand();
                             saveClass = this.$refs.tree.getNodeByKey((classh.cid).toString());
@@ -211,6 +254,8 @@
                              saveClass = this.$refs.tree.getNodeByKey((classh.cid));
                         }
                     });         
+
+                    console.log(this.superClasses);
                     saveClass.select();     
                 
             },
@@ -250,15 +295,12 @@
 
             menuItemSelected: function(item, node) {
                 if (item.code === 'ADD_CHILD_NODE') {
-                    //this.clearNodee();
                     const foundNode = this.allClasses.find( classh => classh.cid == node.data.id);
-                    this.nodee.cid = foundNode.cid;
-                    this.nodee.name = foundNode.name;
-                    this.nodee.pid = foundNode.pid;
+                    this.clearNodee();
+                    this.nodee.pid = foundNode.cid;
                     $('#add_modal').modal('show');
                 }
                 else if (item.code === 'UPDATE_NODE') {
-                    //this.clearNodee();
                     const foundNode = this.allClasses.find( classh => classh.cid == node.data.id);
                     this.nodee = foundNode;
                     $('#update_modal').modal('show');
@@ -266,6 +308,16 @@
                 else if (item.code === 'DELETE_NODEE') {
                     this.deleteNode(node.data.id);
                 }
+            },
+
+            getParentNameById: function(id){
+                try {
+                    const foundNode = this.allClasses.find( classh => classh.cid == id);
+                    return foundNode.name;
+                } catch (error) {
+                    return '"No parent"';
+                }
+
             },
 
             updateNode: function() {
@@ -279,6 +331,8 @@
                 .then(res => res.json())
                 .then(data => {
                     this.fetchSubClasses(this.classCid);
+                    this.fetchAllClasses();
+
                     alert('Node Updated');
                     $('#update_modal').modal('hide');
                     this.clearNodee();
@@ -298,7 +352,15 @@
                 .then(res => this.response = res.json())
                 .then(data => {
                     this.fetchSubClasses(this.classCid);
-                    alert(data.message[0]);
+                    this.fetchAllClasses();
+
+                    if(data.ret == "false"){
+                        alert(data.message[0]);
+                    }
+                    else{
+                        alert("\"" + this.nodee.name + "\" was successfuly added !" );
+                    }
+
                     $('#add_modal').modal('hide');
                     this.clearNodee();
                 })
@@ -306,20 +368,24 @@
             },
 
             deleteNode: function(id) {
-                if (confirm('Are You Sure you want to delete this Node?')) {
+
+                const foundNode = this.allClasses.find( classh => classh.cid == id);
+                if (confirm('Are You Sure you want to delete "'+ foundNode.name +'" Node?')) {
                     fetch(`api/deleteclass/${id}`, {
                     method: 'delete'
                     })
                     .then(res => res.json())
                     .then(data => {
                         this.fetchSubClasses(this.classCid);
-                        alert('Node Removed');
+                        this.fetchAllClasses();
+
+                        alert('Node "'+ foundNode.name +'" was Removed');
                     })
                     .catch(err => console.log(err));
                 }
             },
 
-            clearNodee() {
+            clearNodee: function() {
                 this.nodee.cid = "";
                 this.nodee.name = "";
                 this.nodee.pid = "";
